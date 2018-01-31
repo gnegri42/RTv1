42352473e13d141849941cbea21573d38ab2d751
Benjamin MUSELET <bmuselet@e3r11p16.42.fr>
2018-01-31 18:00:58 +0100

    correcting exit function

--- srcs/exit_properly.c
+++ srcs/exit_properly.c
@@ -11,22 +11,23 @@
 /* ************************************************************************** */
 
 #include <rtv1.h>
-
+/*
 static void	rtv1_free_map(t_mlx *mlx)
 {
 	int				x;
 
 	x = 0;
 	while (x < mlx->map->nb_line)
-		free(mlx->map->content[x++]);
-	mlx->map->sphere ? free(mlx->map->sphere) : (0);
-//	mlx->map->cylindre ? free(mlx->map->cylindre ) : (0);
-	free(mlx->map->str);
-}
+	{
+		free(mlx->map->content[x]);
+		x++;
+	}
+}*/
 
 int			exit_properly(t_mlx *mlx)
 {
-	mlx->map->content ? rtv1_free_map(mlx) : (0);
+	free(mlx->map->str);
+//	mlx->map->content ? rtv1_free_map(mlx) : (0);
 	mlx_destroy_image(mlx->mlx, mlx->img->img);
 	mlx_destroy_window(mlx->mlx, mlx->win);
 	exit(0);
