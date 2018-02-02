936287b702c1cce5772d64d55ce0d8068baaea10
Benjamin MUSELET <bmuselet@e3r11p15.42.fr>
2018-02-02 12:30:02 +0100

    Putting multiple spheres in the same img. Try to find source of the deformation when not at center

--- srcs/sphere.c
+++ srcs/sphere.c
@@ -19,7 +19,6 @@ static int	sphere_intersection(t_mlx *mlx, t_vec3 distance_vector, float project
 	float	new_length;
 
 	distance_sq = vector_dot_product(distance_vector, distance_vector);
-//	radius_sq = mlx->map->sphere->radius * mlx->map->sphere->radius;
 	radius_sq = mlx->map->list->sphere.radius * mlx->map->list->sphere.radius;
 	if (distance_sq > radius_sq)
 		mlx->cam->ray->sphere_intersection = 0;
@@ -36,32 +35,59 @@ static int	sphere_intersection(t_mlx *mlx, t_vec3 distance_vector, float project
 
 int	sphere_intersection_init(t_mlx *mlx, int x, int y)
 {
-/*	t_sphere	*sphere;
-	t_vec3		origin_to_sphere;
-	float		projection;
-	t_vec3		distance_vector;
-
-
-	if (!(sphere = (t_sphere *)malloc(sizeof(t_sphere))))
-		return (-1);
-	mlx->map->sphere = sphere;
-	mlx->cam->ray->sphere_intersection = 0;
-	mlx->map->sphere->radius = 8;
-	sphere->position = vector_assign_values(0, 0, 40);
-	origin_to_sphere = vector_substraction(sphere->position, mlx->cam->ray->origin);
-	projection = vector_dot_product(origin_to_sphere, mlx->cam->ray->direction);
-	distance_vector = vector_substraction(origin_to_sphere, vector_float_product(mlx->cam->ray->direction, projection));
-	sphere_intersection(mlx, distance_vector, projection, x, y);
-	free(sphere);
-	return (0);*/
-	t_vec3		origin_to_sphere;
+	/*t_vec3		origin_to_sphere;
 	float		projection;
 	t_vec3		distance_vector;
 
 	mlx->cam->ray->sphere_intersection = 0;
 	origin_to_sphere = vector_substraction(mlx->map->list->sphere.position, mlx->cam->ray->origin);
-	projection = vector_dot_product(origin_to_sphere, mlx->cam->ray->direction);
+	if ((projection = vector_dot_product(origin_to_sphere, mlx->cam->ray->direction)) < 0)
+		return (0);
 	distance_vector = vector_substraction(origin_to_sphere, vector_float_product(mlx->cam->ray->direction, projection));
-	sphere_intersection(mlx, distance_vector, projection, x, y);
+	sphere_intersection(mlx, distance_vector, projection, x, y);*/
+
+	t_vec3	L;
+	float	a;
+	float	b;
+	float	c;
+	float	discr;
+	float	x0;
+	float	x1;
+	float	q;
+	float	tmp;
+
+	mlx->cam->ray->sphere_intersection = 0;
+
+	L = vector_substraction(mlx->cam->ray->origin, mlx->map->list->sphere.position);
+	a = vector_dot_product(mlx->cam->view_dir, mlx->cam->view_dir);
+	b = 2 * vector_dot_product(mlx->cam->view_dir, L);
+	c = (vector_dot_product(L, L) - (mlx->map->list->sphere.radius * mlx->map->list->sphere.radius));
+	discr = b * b - 4 * a * c;
+	if (discr < 0)
+		return (0);
+	else if (discr == 0)
+	{
+		x0 = -0.5 * b / a;
+		x1 = x0;
+	}
+	else
+	{
+		q = (b > 0) ? -0.5 * (b + sqrt(discr)) : -0.5 * (b - sqrt(discr));
+		x0 = q /a;
+		x1 = c / q;
+	}
+	if (x0 > x1)
+	{
+		tmp = x0;
+		x0 = x1;
+		x1 = tmp;
+	}
+	if (x0 < 0)
+	{
+		x0 = x1;
+		if (x0 < 0)
+			return (0);
+	}
+	mlx->cam->ray->sphere_intersection = 1;
 	return (0);
 }
